ecde492e24b85a02d2bdfd8d1c903768178174ac
Abe Storey <abester101@gmail.com>
2014-02-04 18:39:01 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeatTheWeatherMan::Application.config.secret_key_base = 'f4309f82a1e65fc6f5608dd59bf2d77faf89f0d4e1aa07d3b79c24e79ddf59a1a4534b3a799cd6ca343c8fa0065a9480a112991823b231efcbb4f711aff6bf97'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BeatTheWeatherMan::Application.config.session_store :cookie_store, key: '_BeatTheWeatherMan_session'
